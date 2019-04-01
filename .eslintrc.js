344f6d6b7874e886af8655aee312578a775da0c8
zrobiao <zrobiao@163.com>
2019-04-01 16:24:30 +0800

    change

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,10 @@ module.exports = {
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'space-before-function-paren': 0
+    'space-before-function-paren': 0,
+    "no-const-assign": 2,//禁止修改const声明的变量
+    "no-else-return": 2,//如果if语句里面有return,后面不能跟else语句
+    "no-irregular-whitespace": 2,//不能有不规则的空格
+    "no-label-var": 2,//label名不能与var声明的变量名相同
   }
 }
